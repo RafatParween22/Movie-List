b8085120441b3db5be7b614bd1bc134889cbef01
Rafat Parween <rafat22@navgurukul.org>
2023-08-08 23:45:07 +0530

    GIT repo fixed

---
+++ app/Add_movie.js
@@ -0,0 +1,9 @@
+const Button = ({ Add Movie, onClick }) => {
+  return (
+    <button onClick={onClick}>
+      {Add Movie}
+    </button>
+  );
+};
+
+export default Button;
--- app/Row_of_movie.js
+++ app/Row_of_movie.js
@@ -1,6 +1,6 @@
 import Action_row from './Action_row.js';
 
-function Row_of_movie({movie,onDelete}) {
+function Row_of_movie({movie,onDelete,handleDislike,handleLike,count}) {
   return (
   	<div className="row-of-movie">
       <div className="container">
@@ -14,13 +14,19 @@ function Row_of_movie({movie,onDelete}) {
         </p>
         <h2 className="text">Description</h2>
         <p>{movie.description}</p>
-        <Action_row onDelete={onDelete} />
+        
+        <Action_row 
+		    onDelete={onDelete}
+		    handleLike={handleLike}
+		    handleDislike={handleDislike}
+		    count={count}
+         />
          
       </div>
      
       
  
-    </div>
+    <div>
     	
    
     
